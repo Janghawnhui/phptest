d8b06b927177c2b42476410fe56a6fc8d3117838
unknown <ghksgml0807@gmail.com>
2024-06-09 11:09:59 +0900

    commit2

--- music_festival/footer.php
+++
@@ -1,7 +0,0 @@
-<div id="footer_content">
-            <p id="footer_logo">음악페스티벌 | <span>환희</span></p>
-            <ul id="author">
-                <li>저자 문의 메일</li>
-                <li>- 메일 주소 : ghksgml0807@naver.com</li>
-            </ul>
-        </div>
--- music_festival/main.php
+++
@@ -1,5 +0,0 @@
-<div id="main_img_bar">
-            <img src="./img/main.jpg" height ="200" >
-        </div>
-        <div id="main_content">
-            <div id="latest">
